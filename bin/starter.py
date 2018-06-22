a0863c4ec0fb8938fc96f00ca0b8b82de3d695cf
Nikhilesh2 <nikhilesh2010@live.com>
2018-06-22 16:17:19 -0400

    started on automating server configs

--- bin/starter.py
+++ bin/starter.py
@@ -1,12 +1,10 @@
 import sys
 import json
-from pprint import pprint
 import os
-
+from pprint import pprint
 
 
 OUTPUT_DIR = 'output/'
-
 
 def generateConfig(config):
     with open(os.path.join(OUTPUT_DIR, config['fileName']), 'w') as config_file:
@@ -16,18 +14,24 @@ def generateConfig(config):
 def generateTopicMappings(config):
     with open(os.path.join(OUTPUT_DIR, config['fileName']), 'w') as config_file:
         for mapping in config['mappings']:
-            config_file.write(mapping['srcTopics'] + " " + mapping['destTopic'] + '\n')
+            config_file.write(mapping['srcTopic'] + " " + mapping['destTopic'] + '\n')
+
+def generateServerConfigs(config):
+
         
         
 
 def main():
-    # config = sys.argv[1]
-    config = 'config/config.json'
+    # Configurations for helix controllers/workers
+    controllerConfig = 'config/controllerConfig.json'
+    # Configurations for zookeeper/kafka servers
+    serverConfig = 'config/serverConfig.json'
 
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
 
-    with open(config) as f:
+    # Generate Controller configurations
+    with open(controllerConfig) as f:
         data = json.load(f)
         for controller in data:
             workerConfigs = controller['workerConfig']
@@ -35,4 +39,16 @@ def main():
             generateConfig(workerConfigs['producer'])
             generateConfig(workerConfigs['helix'])
             generateTopicMappings(workerConfigs['topics'])
+    
+ # Generate Server configurations
+    with open(serverConfig) as f:
+        data = json.load(f)
+        for controller in data:
+            workerConfigs = controller['workerConfig']
+            generateConfig(workerConfigs['consumer'])
+            generateConfig(workerConfigs['producer'])
+            generateConfig(workerConfigs['helix'])
+            generateTopicMappings(workerConfigs['topics'])
+    
+    
 main()
