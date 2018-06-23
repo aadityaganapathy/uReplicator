bf763f28066a36aae2dfe27d4ac6e768d036cd4b
Nikhilesh2 <nikhilesh2010@live.com>
2018-06-22 21:44:10 -0400

    finish zoo/broker server creation

--- bin/starter.py
+++ bin/starter.py
@@ -2,26 +2,30 @@ import sys
 import json
 import os
 from pprint import pprint
+from subprocess import call
+import subprocess
+import platform
+import time
 
 
 OUTPUT_DIR = 'output/'
+KAFKA_LOGS = '/tmp/kafka-logs/'
 
 def generateConfig(config):
     with open(os.path.join(OUTPUT_DIR, config['fileName']), 'w') as config_file:
         for attr, val in config.items():
-            config_file.write(attr + "=" + val + '\n')
+            config_file.write(str(attr) + "=" + str(val) + '\n')
 
 def generateTopicMappings(config):
     with open(os.path.join(OUTPUT_DIR, config['fileName']), 'w') as config_file:
         for mapping in config['mappings']:
             config_file.write(mapping['srcTopic'] + " " + mapping['destTopic'] + '\n')
 
-def generateServerConfigs(config):
+# def generateServerConfigs(config):
 
-        
-        
+    
 
-def main():
+def generate_config():
     # Configurations for helix controllers/workers
     controllerConfig = 'config/controllerConfig.json'
     # Configurations for zookeeper/kafka servers
@@ -30,25 +34,104 @@ def main():
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
 
+     # Generate Server configurations
+    with open(serverConfig) as f:
+        data = json.load(f)
+        for server in data:
+            zoo_conf = server['zooConfig']
+            ZOO_PATH = zoo_conf['clientPort'] + "_cluster"
+            if not os.path.exists(OUTPUT_DIR + "/" + ZOO_PATH):
+                os.makedirs(OUTPUT_DIR + "/" + ZOO_PATH)
+            # Generate readable zookeeper filename
+            zoo_conf['fileName'] = ZOO_PATH + "/" + "zoo_" + zoo_conf['clientPort'] + ".properties"
+            # Generate zookeeper config
+            generateConfig(zoo_conf)
+
+            # Loop through each broker managed by zookeeper
+            for broker in server['brokersConfig']:
+                # Generate readable kafka broker filename
+                broker['fileName'] = ZOO_PATH + "/" + str("broker_" + str(server['zooConfig']['clientPort']) + "_" + str(broker['broker.id']) + ".properties")
+
+                # Add the corresponding zookeeper listener
+                broker['zookeeper.connect']= "localhost:" + zoo_conf['clientPort']
+
+                # Set path for logs
+                broker['log.dirs'] = KAFKA_LOGS + ZOO_PATH + "_" + str(broker['broker.id'])
+                # Generate broker config
+                generateConfig(broker)
+
     # Generate Controller configurations
     with open(controllerConfig) as f:
         data = json.load(f)
         for controller in data:
+            controller_id = controller['workerConfig']['consumer']['zookeeper.connect'].split(':')[1]
+            ZOO_PATH = zoo_conf['clientPort'] + "_cluster"
+            if not os.path.exists(OUTPUT_DIR + "/" + ZOO_PATH):
+                print(OUTPUT_DIR + "/" + ZOO_PATH + "does not exist! Exiting")
+                exit(1)
+
+            if not os.path.exists(OUTPUT_DIR + ZOO_PATH + "/worker/"):
+                os.makedirs(OUTPUT_DIR + ZOO_PATH + "/worker/")
+
             workerConfigs = controller['workerConfig']
+           
+            workerConfigs['consumer']['fileName'] = ZOO_PATH + '/worker/consumer.properties'
+            workerConfigs['producer']['fileName'] = ZOO_PATH + '/worker/producer.properties'
+            workerConfigs['helix']['fileName'] = ZOO_PATH + "/worker/helix.properties"
+            workerConfigs['topics']['fileName'] = ZOO_PATH + "/worker/topic.mappings"
+
             generateConfig(workerConfigs['consumer'])
             generateConfig(workerConfigs['producer'])
             generateConfig(workerConfigs['helix'])
             generateTopicMappings(workerConfigs['topics'])
     
- # Generate Server configurations
-    with open(serverConfig) as f:
-        data = json.load(f)
-        for controller in data:
-            workerConfigs = controller['workerConfig']
-            generateConfig(workerConfigs['consumer'])
-            generateConfig(workerConfigs['producer'])
-            generateConfig(workerConfigs['helix'])
-            generateTopicMappings(workerConfigs['topics'])
+
+# TODO: GET THIS TO WORK
+def terminate_clusters():
+    call(['dos2unix', 'bin/stop_clusters.sh'])
+    p = subprocess.Popen(['sh','bin/stop_clusters.sh'])
+    time.sleep(2)
+    p.kill()
+
+# Go through output folder and run all clusters       
+def run_clusters():
+    cluster_dirs = next(os.walk('output'))[1]
+    brokers = []
+    for cluster in cluster_dirs:
+        zoo_properties = ''
+        sub_obj = os.listdir(OUTPUT_DIR + cluster)
+        for item in sub_obj:
+            f_type = item.split('_')[0]
+            if f_type == 'broker':
+                brokers.append(item)
+            elif f_type == 'zoo':
+                zoo_properties = item
+        print("Running ZooKeeper On Port: " + zoo_properties.split('_')[1].split('.')[0])
+        run_zoo(OUTPUT_DIR + cluster + '/' + zoo_properties)
     
+    for broker in brokers:
+        print("Running Broker under ZooKeeper: " + broker.split('_')[1] + " | broker id: " + broker.split('_')[2].split('.')[0])
+        path_to_broker = OUTPUT_DIR + cluster + '/' + broker
+        if platform.system() == 'Windows':
+            bash_exe = "C:/cygwin64/bin/bash.exe" 
+            call([bash_exe, './deploy/kafka/bin/kafka-server-start.sh', '-daemon', path_to_broker])
+        else:
+            call(['./deploy/kafka/bin/kafka-server-start.sh', '-daemon', path_to_broker])
+
     
+
+def run_zoo(path_to_config):
+    # call(['./deploy/kafka/bin/zookeeper-server-start.sh', '-daemon', path_to_config])
+    if platform.system() == 'Windows':
+        bash_exe = "C:/cygwin64/bin/bash.exe" 
+        call([bash_exe, './deploy/kafka/bin/zookeeper-server-start.sh', '-daemon', path_to_config])
+    else:
+        call(['./deploy/kafka/bin/zookeeper-server-start.sh', '-daemon', path_to_config])
+
+def main():
+    generate_config()
+    # terminate_clusters()
+    run_clusters()
+    
+
 main()
---
+++ bin/stop_clusters.sh
@@ -0,0 +1,18 @@
+ #!/bin/sh
+ 
+ echo "Clearing Kafka Logs..."
+  rm -rf /tmp/kafka-logs/
+  echo "Finished Clearing Kafka Logs!"
+  # $DEPLOY_ROOT_DIR/kafka/bin/kafka-server-stop.sh
+  value=`jps | grep Quorum`
+  a=($(echo "$value" | tr ' ' '\n'))
+
+  re='^[0-9]+$'
+  
+  for i in "${a[@]}"; do
+    if  [[ $i =~ $re ]] ; then
+      echo "Stopping Zookeeper with PID: ${i}"
+      kill -9 $i
+    fi
+  done
+  exit
